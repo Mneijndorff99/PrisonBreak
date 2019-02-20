82e2bfd892c711a53b150c61e382841bf22bbe45
Mneijndorff <33960298+Mneijndorff99@users.noreply.github.com>
2019-02-20 16:13:43 +0100

    Added AI and NavMesh, also changed some prefabs

---
+++ Assets/Scripts/AI/AIMovement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIMovement : MonoBehaviour
+{
+    public Animator ani;
+    public NavMeshAgent navMesh;
+    public Transform target;
+
+    private void Update()
+    {
+        navMesh.SetDestination(new Vector3(target.position.x, target.position.y, target.position.z));
+        ani.SetBool("walking", true);
+    }
+}
